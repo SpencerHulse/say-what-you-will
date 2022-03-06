8a7bb146f0d11f7c10d3c070147ca3243d3c2a8b
Spencer Hulse <hulse.spencer@gmail.com>
2022-03-06 00:13:06 -0500

    Created database and connected to it.

---
+++ db/schema.sql
@@ -0,0 +1,3 @@
+DROP DATABASE IF EXISTS say_what_you_will_db;
+
+CREATE DATABASE say_what_you_will_db;
--- server.js
+++ server.js
@@ -4,26 +4,28 @@ const app = express();
 
 const path = require("path");
 
+// Sequelize
+const sequelize = require("./config/connection");
+
 // Handlebars templates
 const exphbs = require("express-handlebars");
 const hbs = exphbs.create({
   /* any custom helpers go here */
 });
-
-const routes = require("./controllers/index");
-
 // Set the handlebars engine
 app.set("view engine", "handlebars");
 app.engine("handlebars", hbs.engine);
 
 // Middleware
 app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+app.use(express.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
-// Routes
-app.use("/", routes);
+// Routes - Directly require it in the app.use()
+app.use(require("./controllers/index"));
 
 // Server
 const PORT = process.env.PORT || 3001;
-app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+sequelize.sync({ force: false }).then(() => {
+  app.listen(PORT, () => console.log(`Listening on port ${PORT}...`));
+});
