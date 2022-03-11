f049304bfc2287bafa01a464ac19a16acc27fd6c
Spencer Hulse <hulse.spencer@gmail.com>
2022-03-10 20:48:20 -0500

    Added route and template for single posts.

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -34,4 +34,38 @@ router.get("/", (req, res) => {
     });
 });
 
+// Get a single post for the single-post page
+router.get("/post/:id", (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ["id", "title", "post_text", "created_at"],
+    include: [
+      {
+        model: Comment,
+        attributes: ["id", "comment_text", "post_id", "user_id", "created_at"],
+        include: {
+          model: User,
+          attributes: ["username"],
+        },
+      },
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((data) => {
+      const post = data.get({ plain: true });
+
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
 module.exports = router;
