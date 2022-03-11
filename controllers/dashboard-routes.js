6571551ee730858a4681cb00037594ac671cfda1
Spencer Hulse <hulse.spencer@gmail.com>
2022-03-11 03:45:18 -0500

    Added dashboard template and route.

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,40 @@
+const router = require("express").Router();
+const { Post, Comment, User } = require("../models");
+
+// Get all posts for the user
+router.get("/", (req, res) => {
+  Post.findAll({
+    where: { user_id: 1 /* req.session.user.id */ },
+    attributes: ["id", "title", "created_at"],
+  })
+    .then((data) => {
+      const posts = data.map((post) => post.get({ plain: true }));
+      res.render("dashboard", { posts, loggedIn: req.session.loggedIn });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+// Get single post to edit or delete
+router.get("/:id", (req, res) => {
+  Post.findOne({
+    where: { id: req.params.id },
+    attributes: ["id", "title", "post_text"],
+  })
+    .then((data) => {
+      const post = data.get({ plain: true });
+
+      res.render("single-post", {
+        post,
+        loggedIn: req.session.loggedIn,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
--- controllers/index.js
+++ controllers/index.js
@@ -1,9 +1,11 @@
 const router = require("express").Router();
-const homeRoutes = require("./home-routes");
 const apiRoutes = require("./api/index");
+const homeRoutes = require("./home-routes");
+const dashboardRoutes = require("./dashboard-routes");
 
-router.use("/", homeRoutes);
 router.use("/api", apiRoutes);
+router.use("/", homeRoutes);
+router.use("/dashboard", dashboardRoutes);
 
 // Catches any other routes
 router.use((req, res) => {
